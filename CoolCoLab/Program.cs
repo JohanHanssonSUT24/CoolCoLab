93bea23ddb7ceb89c80ce717b1dc686076b2803a
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-09-17 11:39:45 +0200

    Add project files.

---
+++ CoolCoLab/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CoolCoLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my co-lab, its freaking cool!");
+        }
+    }
+}
